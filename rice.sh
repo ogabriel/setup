1a0f882674d48631533c2af84a8115fbfe604240
ogabriel <gabrieloliver8991@gmail.com>
2019-05-01 16:07:28 -0300

    remodeling rice script

--- rice.sh
+++ rice.sh
@@ -7,7 +7,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install -y dialog
 cmd=(dialog --separate-output --checklist "Please select software you want to install: " 22 76 16)
 options=(
-	1 "pywal" on
+	1 "basic" on
+	2 "pywal" on
 	2 "polybar" on
 	)
 	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
@@ -16,12 +17,17 @@ for choice in $choices
 do
 case $choice in
     1)
+	sudo apt-get install i3 xorg network-mananger-gnome neofetch \
+	ranger thunar chromium pulseaudio blueman \
+	rxvt-unicode sudo feh build-essentials lm-sensors
+	;;
+    2)
 	sudo apt-get install python3 python3-pip
 	sudo pip3 install pywal
 
 	echo '(cat ~/.cache/wal/sequences &)' >> /home/$USER/.bashrc
 	;;
-    2)
+    3)
 	cdc
 	sudo apt-get install build_essentials
 
