73c87df1849fe496498e5f6699746999cf634d91
Gabriel <gabrieloliver8991@gmail.com>
2019-05-03 17:37:17 -0300

    adding docker-compose

--- dev.sh
+++ dev.sh
@@ -12,11 +12,12 @@ options=(
         3 "neovim" off
         4 "vscode" off
         5 "docker-ce" off
-        6 "postman" off
-        7 "kubectl" off
-        8 "click 0.3.2" off
-        9 "gitkraken" off
-        10 "dbeaver" off
+        6 "docker-composer" off
+        7 "postman" off
+        8 "kubectl" off
+        9 "click 0.3.2" off
+        10 "gitkraken" off
+        11 "dbeaver" off
         )
         choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         clear
@@ -64,10 +65,18 @@ do
             sudo apt-get update
             sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+            # use without sudo
             sudo groupadd docker
             sudo usermod -aG docker $USER
+
+            sudo systemctl enable docker
+            echo manual | sudo tee /etc/init/docker.override
             ;;
         6)
+            sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+            sudo chmod +x /usr/local/bin/docker-compose
+            ;;
+        7)
             wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
             sudo tar -xzf postman.tar.gz -C /opt
             rm postman.tar.gz
@@ -84,14 +93,14 @@ Type=Application
 Categories=Development;
 EOL
 ;;
-        7)
+        8)
             sudo apt-get update && sudo apt-get install -y apt-transport-https
             curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
             echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
             sudo apt-get update
             sudo apt-get install -y kubectl
             ;;
-        8)
+        9)
             echo "this installation require manually installation of rust C-c to cancel (https://www.rust-lang.org/tools/install)"
             read
             # cloning click and installing click
@@ -104,13 +113,13 @@ EOL
             echo "alias click=~/.click/target/release/click" >> ~/.bashrc
             cd
             ;;
-        9)
+        10)
             sudo apt-get install gvfs-bin
             wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
             sudo dpkg -i gitkraken-amd64.deb
             rm gitkraken-amd64.deb
             ;;
-        10)
+        11)
             sudo add-apt-repository ppa:serge-rider/dbeaver-ce
             sudo apt-get update
             sudo apt-get install -y dbeaver-ce
