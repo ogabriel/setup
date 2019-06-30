301f23e4fc6bfd25ec0d1aa0deb87a1f711b7a9a
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-06-29 23:53:31 -0300

    updating angel

--- installers/angel.sh
+++ installers/angel.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-sudo apt install neofetch
+sudo apt install neofetch -y
 
-sudo apt install git
+sudo apt install git -y
 
-sudo apt install xserver-xorg-video-dummy xserver-xorg-input-void xserver-xorg-core xinit x11-xserver-utils
+sudo apt install xserver-xorg-video-dummy xserver-xorg-input-void xserver-xorg-core xinit x11-xserver-utils -y
 
-sudo apt install xserver-xorg-video-nouveau
+sudo apt install xserver-xorg-video-nouveau -y
 
-sudo apt install xserver-xorg-input-mouse xserver-xorg-input-kbd xserver-xorg-inputs-synaptics
+sudo apt install xserver-xorg-input-mouse xserver-xorg-input-kbd xserver-xorg-inputs-synaptics -y
 
-sudo apt install xfonts-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
+sudo apt install xfonts-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable -y
 
-sudo apt install libgl1-mesa-dri mesa-utils
+sudo apt install libgl1-mesa-dri mesa-utils -y
 
-sudo apt install lightdm
+sudo apt install lightdm --no-install-recommends -y
+
+sudo apt install lightdm-greeter -y
+
+apt install i3 --no-install-recommends -y
+
+apt install compton -y
+
+apt install vim neovim vim-gnome -y
+
