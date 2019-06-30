d3c7a781437d0f991ad7b35c5a18bf72af76b28e
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-06-30 01:51:32 -0300

    really working now

--- installers/angel.sh
+++ installers/angel.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo apt install linux-headers-$(uname -r)
+
 sudo apt install neofetch -y
 
 sudo apt install git -y
 
-sudo apt install xserver-xorg-video-dummy xserver-xorg-input-void xserver-xorg-core xinit x11-xserver-utils -y
+sudo apt install xserver-xorg-video-dummy xserver-xorg-input-void xinit x11-xserver-utils -y
 
 sudo apt install xserver-xorg-video-nouveau -y
 
@@ -12,13 +14,11 @@ sudo apt install xserver-xorg-input-mouse xserver-xorg-input-kbd xserver-xorg-in
 
 sudo apt install xfonts-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable -y
 
+sudo apt install xserver-xorg-hwe-18.04 xserver-xorg-input-all-hwe-18.04 -y
+
 sudo apt install libgl1-mesa-dri mesa-utils -y
 
-sudo apt install lightdm --no-install-recommends -y
-
-dm-tools --test-mode -d
-
-sudo apt install lightdm-greeter lightdm-webkit-greeter -y
+sudo apt install lightdm lightdm-greeter lightdm-webkit-greeter --no-install-recommends -y
 
 apt install i3 --no-install-recommends -y
 
@@ -26,3 +26,16 @@ apt install compton -y
 
 apt install vim neovim vim-gnome -y
 
+apt install chromium-browser -y
+
+apt install build-essential -y
+
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your PATH)
+ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
+# Update the path to the kitty icon in the kitty.desktop file
+sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
