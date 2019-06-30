db2cacc7828153a0cc5f118d5ef2b3acb85b0607
ogabriel <gabrieloliver8991@gmail.com>
2019-06-30 13:19:29 -0300

    remove kitty from angel

--- installers/angel.sh
+++ installers/angel.sh
@@ -34,12 +34,4 @@ sudo apt install build-essential -y
 
 sudo apt install pulseaudio feh imagemagick ranger chromium-browser
 
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
-# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
-# your PATH)
-ln -s ~/.local/kitty.app/bin/kitty ~/.local/bin/
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
-# Update the path to the kitty icon in the kitty.desktop file
-sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
