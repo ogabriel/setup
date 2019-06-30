be74eb719337ad453925d20cdc96e194c9dfbf66
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2019-06-29 22:58:22 -0300

    add angel file

---
+++ installers/angel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt install neofetch
+
+sudo apt install git
+
+sudo apt install xserver-xorg-video-dummy xserver-xorg-input-void xserver-xorg-core xinit x11-xserver-utils
+
+sudo apt install xserver-xorg-video-nouveau
+
+sudo apt install xserver-xorg-input-mouse xserver-xorg-input-kbd xserver-xorg-inputs-synaptics
+
+sudo apt install xfonts-utils xfonts-100dpi xfonts-75dpi xfonts-base xfonts-scalable
+
+sudo apt install libgl1-mesa-dri mesa-utils
+
+sudo apt install lightdm
