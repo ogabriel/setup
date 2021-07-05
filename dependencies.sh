947d54120e8ce38bd322b11b635b28d87aa8d861
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2021-07-04 23:31:03 -0300

    remove old dependencies

--- dependencies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# some dependencies
-
-sudo apt-get update && sudo apt-get upgrade -y
-
-sudo apt-get install -y curl wget software-properties-common build-essential libyaml-dev libssl-dev libgdbm-dev \
-		libncurses5-dev libpq-dev libffi-dev make gcc g++ autoconf bison libyaml-dev libreadline7  \
-		libreadline-dev zlib1g zlib1g-dev openssl libssl-dev libssl-dev libyaml-dev
