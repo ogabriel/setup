3e095c2f2e2b5523b272cb98af4355335a3a545d
ogabriel <gabrieloliver8991@gmail.com>
2022-08-19 21:28:26 -0300

    fix install

--- cura/install.sh
+++ cura/install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-mkdir -p /opt/cura
+sudo mkdir -p /opt/cura
 
-cp ./cura.png /opt/cura
+sudo cp ./cura.png /opt/cura
 
-wget -O /opt/cura/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
-chmod +x /opt/cura/cura
+sudo wget -O /opt/cura/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
+sudo chmod +x /opt/cura/cura
 
 mkdir -p ~/.local/share/applications/
 cp ./cura.desktop ~/.local/share/applications/
-
-mkdir -p ~/.local/bin
-ln -s /opt/cura/cura ~/.local/bin/cura
