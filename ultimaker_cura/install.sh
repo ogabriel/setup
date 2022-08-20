7b80d3c3e5a5c02f3f59876356eebbc7686bd663
ogabriel <gabrieloliver8991@gmail.com>
2022-08-19 21:18:09 -0300

    update

--- ultimaker_cura/install.sh
+++ ultimaker_cura/install.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /opt/cura
 
-cp ./ultimaker_cura.png /opt/cura
+cp ./cura.png /opt/cura
 
-wget -O cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage /opt/cura/
+wget -O /opt/cura/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
 chmod +x /opt/cura/cura
 
 mkdir -p ~/.local/share/applications/
-cp ./ultimaker_cura.desktop ~/.local/share/applications/
+cp ./cura.desktop ~/.local/share/applications/
 
 mkdir -p ~/.local/bin
 ln -s /opt/cura/cura ~/.local/bin/cura
