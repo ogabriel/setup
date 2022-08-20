9b29936eb001b8bc7c47409df89a62abebc5632d
ogabriel <gabrieloliver8991@gmail.com>
2022-08-19 21:07:36 -0300

    add install

---
+++ ultimaker_cura/install.sh
@@ -0,0 +1,10 @@
+
+mkdir -p /opt/cura
+
+cp ./ultimaker_cura.png /opt/cura
+cp ./ultimaker_cura.png /opt/cura
+
+wget -O cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage /opt/cura/
+chmod +x /opt/cura/cura
+
+
