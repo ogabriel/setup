b9dab47fcbf68a02b5aaddba6b435079c7892806
ogabriel <gabrieloliver8991@gmail.com>
2022-09-15 09:19:20 -0300

    add comments to cura install

--- cura/install.sh
+++ cura/install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# create folder
 sudo mkdir -p /opt/cura
 
+# add icon
 sudo cp ./icon.png /opt/cura
 
+# create shortcut
+mkdir -p ~/.local/share/applications/
+cp ./cura.desktop ~/.local/share/applications/
+
+# download appimage
 sudo wget -O /tmp/cura.AppImage https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
 chmod +x /tmp/cura.AppImage
 
 sudo mv /tmp/cura.AppImage /opt/cura/
-
-mkdir -p ~/.local/share/applications/
-cp ./cura.desktop ~/.local/share/applications/
