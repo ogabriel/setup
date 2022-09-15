e993fa150b67be451649fe195e1bfc2cfb66846e
ogabriel <gabrieloliver8991@gmail.com>
2022-09-15 09:13:59 -0300

    change icon name on cura

--- cura/install.sh
+++ cura/install.sh
@@ -2,7 +2,7 @@
 
 sudo mkdir -p /opt/cura
 
-sudo cp ./cura.png /opt/cura
+sudo cp ./icon.png /opt/cura
 
 sudo wget -O /tmp/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
 chmod +x /tmp/cura
