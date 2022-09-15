93ad226665bc923ea67f37a1628e3c2d018a971f
ogabriel <gabrieloliver8991@gmail.com>
2022-09-15 09:15:29 -0300

    add .AppImage extension name

--- cura/install.sh
+++ cura/install.sh
@@ -4,10 +4,10 @@ sudo mkdir -p /opt/cura
 
 sudo cp ./icon.png /opt/cura
 
-sudo wget -O /tmp/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
-chmod +x /tmp/cura
+sudo wget -O /tmp/cura.AppImage https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
+chmod +x /tmp/cura.AppImage
 
-sudo mv /tmp/cura /opt/cura/
+sudo mv /tmp/cura.AppImage /opt/cura/
 
 mkdir -p ~/.local/share/applications/
 cp ./cura.desktop ~/.local/share/applications/
