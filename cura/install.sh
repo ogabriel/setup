908c55d2c3f6fa61a38aa5cc95191c136112df65
ogabriel <gabrieloliver8991@gmail.com>
2022-09-15 09:13:07 -0300

    change install method

--- cura/install.sh
+++ cura/install.sh
@@ -4,8 +4,10 @@ sudo mkdir -p /opt/cura
 
 sudo cp ./cura.png /opt/cura
 
-sudo wget -O /opt/cura/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
-sudo chmod +x /opt/cura/cura
+sudo wget -O /tmp/cura https://github.com/Ultimaker/Cura/releases/download/5.1.0/Ultimaker-Cura-5.1.0-linux.AppImage
+chmod +x /tmp/cura
+
+sudo mv /tmp/cura /opt/cura/
 
 mkdir -p ~/.local/share/applications/
 cp ./cura.desktop ~/.local/share/applications/
