b23acbb0a5b7f8eabc927b9467184db5bdc28487
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2023-03-26 11:37:26 -0300

    update prusa slicer

--- 3dprinting/PrusaSlicer/install.sh
+++ 3dprinting/PrusaSlicer/install.sh
@@ -11,7 +11,7 @@ mkdir -p ~/.local/share/applications/
 cp ./prusaslicer.desktop ~/.local/share/applications/
 
 # download appimage
-wget -O /tmp/prusaslicer.AppImage https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.6.0-alpha4/PrusaSlicer-2.6.0-alpha4+linux-x64-GTK3-202302180033.AppImage
+wget -O /tmp/prusaslicer.AppImage https://github.com/prusa3d/PrusaSlicer/releases/download/version_2.6.0-alpha5/PrusaSlicer-2.6.0-alpha5+linux-x64-GTK3-202303061441.AppImage
 
 chmod +x /tmp/prusaslicer.AppImage
 
