db29daa7f571d2f84f17941ce10e96bd199d462d
ogabriel <gabrieloliver8991@gmail.com>
2023-06-10 11:44:12 -0300

    asdf

--- dev/asdf.sh
+++ dev/asdf.sh
@@ -9,13 +9,3 @@ fi
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
-
-CURRENT_SHELL=$(ps | grep `echo $$` | awk '{ print $4 }')
-
-if [ $CURRENT_SHELL == "zsh" ]; then
-    source ~/.zshrc
-elif [ $CURRENT_SHELL == "bash" ]; then
-    source ~/.bashrc
-fi
-
-asdf update
