1dbaee25f39dca6c9302fa0770a68129f4bcdb5e
ogabriel <gabrieloliver8991@gmail.com>
2023-06-10 12:13:34 -0300

    add ruby asdf script

---
+++ dev/asdf/ruby.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if which apt; then
+    sudo apt-get install autoconf patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev -y
+elif which pacman; then
+    # maybe is needed
+    # yay -S gcc6 --noconfirm
+    sudo pacman -S --needed base-devel rust libffi libyaml openssl zlib --noconfirm
+fi
+
+asdf update
+
+asdf plugin add ruby
+
+asdf plugin update ruby
