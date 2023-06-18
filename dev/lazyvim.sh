4735e0bdce0f765f54f30d18b0691fcca7fd0ab3
ogabriel <gabrieloliver8991@gmail.com>
2023-06-18 17:55:46 -0300

    add lazyvim install script

---
+++ dev/lazyvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# required
+mv ~/.config/nvim ~/.config/nvim.bak
+
+# optional but recommended
+mv ~/.local/share/nvim ~/.local/share/nvim.bak
+mv ~/.local/state/nvim ~/.local/state/nvim.bak
+mv ~/.cache/nvim ~/.cache/nvim.bak
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
