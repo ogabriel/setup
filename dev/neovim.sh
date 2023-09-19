3a8093377b7d73815e9ae1849db00133f07b2dd2
ogabriel <gabrieloliver8991@gmail.com>
2023-09-19 14:53:28 -0300

    update neovim script

--- dev/neovim.sh
+++ dev/neovim.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-# #
+
+set -e
+
 if which apt; then
     sudo apt-get install ninja-build gettext cmake unzip curl
 elif which pacman; then
     sudo pacman -S base-devel cmake unzip ninja curl
 fi
 
-set -e
-
 cd /tmp
 rm -rf /tmp/neovim
-
-sudo apt-get install ninja-build gettext cmake unzip curl
 
 git clone https://github.com/neovim/neovim
 
