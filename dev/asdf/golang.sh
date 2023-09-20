9dae919455c03e37f5535fab8aa6bf75d58a9349
ogabriel <gabrieloliver8991@gmail.com>
2023-09-20 09:44:05 -0300

    add golang install

---
+++ dev/asdf/golang.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f "/etc/arch-release" ]; then
+    sudo pacman -S coreutils curl
+elif [ -f "/etc/lsb-release" ]; then
+    sudo apt install coreutils curl
+fi
+
+asdf plugin add golang
+asdf plugin update golang
+asdf install golang latest
+asdf global golang latest
