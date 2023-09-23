d0a275cd06eb5fc4304a35aee291af6a46c57ae1
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 08:31:23 -0300

    update lua

--- dev/asdf/lua.sh
+++ dev/asdf/lua.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ -f "/etc/arch-release" ]; then
-    sudo pacman -S base-devel linux-headers --neededl
+    sudo pacman -S base-devel linux-headers --noconfirm --needed
 elif [ -f "/etc/lsb-release" ]; then
     sudo apt-get install linux-headers-$(uname -r) build-essential
 fi
