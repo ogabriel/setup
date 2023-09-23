e02fb744925d9ba3b8110b5c6f4cc80155ac062a
ogabriel <gabrieloliver8991@gmail.com>
2023-09-23 08:22:33 -0300

    add stylua

---
+++ dev/formatter/stylua.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -f "/etc/arch-release" ]; then
+    sudo pacman -S curl unzip bash
+elif [ -f "/etc/lsb-release" ]; then
+    sudo apt-get install curl unzip bash
+fi
+
+asdf plugin add stylua
+asdf plugin update stylua
+asdf install stylua latest
+asdf global stylua latest
