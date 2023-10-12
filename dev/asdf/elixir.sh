689cb3825b26d6fb83646d8f5ceacd7d3182187e
ogabriel <gabrieloliver8991@gmail.com>
2023-10-12 11:36:42 -0300

    add version to elixir

--- dev/asdf/elixir.sh
+++ dev/asdf/elixir.sh
@@ -4,18 +4,13 @@ if which apt; then
     sudo apt-get install unzip inotify-tools -y
 elif which pacman; then
     sudo pacman -S unzip inotify-tools --noconfirm --needed
-
 fi
-
-asdf update
-
-asdf plugin add elixir
-
-asdf plugin update elixir
 
 # compatibility list for erlang and elixir
 # https://hexdocs.pm/elixir/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
+VERSION=${1:-$(asdf latest elixir)}
 
-asdf install elixir latest
-
-asdf global elixir latest
+asdf plugin add elixir
+asdf plugin update elixir
+asdf install elixir $VERSION
+asdf global elixir $VERSION
