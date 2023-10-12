6b4fa0ba6a7afc597e4fd0b514a74e61b6bfb35f
ogabriel <gabrieloliver8991@gmail.com>
2023-10-12 11:50:28 -0300

    add comment and specify version

--- dev/asdf/elixir.sh
+++ dev/asdf/elixir.sh
@@ -6,11 +6,17 @@ elif which pacman; then
     sudo pacman -S unzip inotify-tools --noconfirm --needed
 fi
 
-# compatibility list for erlang and elixir
-# https://hexdocs.pm/elixir/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
-VERSION=${1:-$(asdf latest elixir)}
-
 asdf plugin add elixir
 asdf plugin update elixir
+
+# compatibility list for erlang and elixir:
+# https://hexdocs.pm/elixir/compatibility-and-deprecations.html#compatibility-between-elixir-and-erlang-otp
+# Elixir precompiled versions:
+# the default OTP version of a raw elixir install will always be an older/oldest version, so prefer to specify the
+# OTP version when installing elixir, as it allows to use the most advanced features of OTP, examples:
+# 1.14.4 = 1.14.4-otp-23 (oldest)
+# 1.14.5 = 1.14.5-otp-24 (older, but not the oldest)
+VERSION=${1:-$(asdf list all elixir | grep -v master | grep -v main | tail -n 1)}
+
 asdf install elixir $VERSION
 asdf global elixir $VERSION
