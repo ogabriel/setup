e14e158dbf61c065e7d87932896940a94822bad1
ogabriel <gabrieloliver8991@gmail.com>
2023-10-12 11:33:05 -0300

    fix java script and add version option

--- dev/asdf/java.sh
+++ dev/asdf/java.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
 if [ -f "/etc/arch-release" ]; then
-    sudo pacman -S --noconfirm bash curl sha256sum unzip jq
+    sudo pacman -S \
+        --noconfirm \
+        --needed \
+        bash \
+        curl \
+        coreutils \
+        unzip \
+        jq
 elif [ -f "/etc/lsb-release" ]; then
     sudo apt-get install bash curl sha256sum unzip jq
 fi
 
+VERSION=${1:-$(asdf list all java | grep openjdk-17 | tail -n 1)}
+
 asdf plugin add java
 asdf plugin update java
-
-LAST_OPENJDK_17_VERSION=$(asdf list all java | grep openjdk-16 | tail -n 1)
-
-asdf install java $LAST_OPENJDK_17_VERSION
-asdf global java $LAST_OPENJDK_17_VERSION
+asdf install java $VERSION
+asdf global java $VERSION
