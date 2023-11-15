325a1914b4ca59341f487372dc258749df767582
ogabriel <gabrieloliver8991@gmail.com>
2023-11-15 08:04:41 -0300

    fix: yay

--- arch/yay.sh
+++ arch/yay.sh
@@ -3,7 +3,7 @@
 if [ -z "$(command -v yay)" ]; then
     echo "Installing yay..."
 
-    sudo pacman -S --needed --no-confirm git base-devel
+    sudo pacman -S --needed --noconfirm git base-devel
 
     cd /tmp
     git clone https://aur.archlinux.org/yay-bin.git
