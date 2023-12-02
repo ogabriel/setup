e38e5243373d4d4e416d5ed9c48ca4c36e75a82f
ogabriel <gabrieloliver8991@gmail.com>
2023-12-02 11:07:06 -0300

    feat: update lua asdf

--- dev/asdf/lua.sh
+++ dev/asdf/lua.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 if [ -f "/etc/arch-release" ]; then
-    sudo pacman -S --noconfirm --needed base-devel linux-headers
+    sudo pacman -Sy --noconfirm --needed base-devel linux-headers
 elif [ -f "/etc/lsb-release" ]; then
     sudo apt install -y linux-headers-$(uname -r) build-essential
 fi
 
 asdf plugin add lua
 asdf plugin update lua
-asdf install lua latest
-asdf global lua latest
+
+VERSION=${1:-$(asdf latest lua)}
+
+asdf install lua $VERSION
+asdf global lua $VERSION
