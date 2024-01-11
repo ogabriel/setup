33e5e48c6472a2296a53a6e3825ea36ca673f5d2
ogabriel <gabrieloliver8991@gmail.com>
2024-01-11 19:10:17 -0300

    update thinkfan install

--- utils/thinkfan.sh
+++ utils/thinkfan.sh
@@ -3,5 +3,6 @@
 if which apt; then
     sudo apt install -y thinkfan
 elif which pacman; then
-    yay -S --noconfirm --needed thinkfan thinkfan-ui
+    yay -S --noconfirm --needed thinkfan thinkfan-ui lm_sensors
+    sudo ln -s libyaml-cpp.so.0.8 /usr/lib/libyaml-cpp.so.0.7
 fi
